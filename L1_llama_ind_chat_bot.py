d0ec97b27f5422d1dca008b68fe457ebe285577a
Sercan Gul <sercan.gul@gmail.com>
2024-02-27 18:51:18 -0600

    updated

---
+++ L1_llama_ind_chat_bot.py
@@ -0,0 +1,38 @@
+# “I want a chatbot instead of Q&A
+
+#See https://docs.llamaindex.ai/en/stable/module_guides/deploying/chat_engines/usage_pattern.html
+#Learn more about the chat engine.
+
+import logging
+import sys
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler(stream=sys.stdout))
+
+import os.path
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+# check if storage already exists
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    # load the documents and create the index
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    # store it for later
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    # load the existing index
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+query_engine = index.as_chat_engine()
+response = query_engine.chat("What did the author do growing up?")
+print(response)
+
+response = query_engine.chat("Oh interesting, tell me more.")
+print(response)
